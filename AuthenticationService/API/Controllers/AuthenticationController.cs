6527cdd6109d25164a2f6ccd484e084240812f02
ivanlmpk <ivanlempek@hotmail.com>
2024-09-01 23:38:57 -0300

    feat: Adicionado controller da parte de autenticação. Adicionado end-point de registro de usuário.

---
+++ AuthenticationService/API/Controllers/AuthenticationController.cs
@@ -0,0 +1,47 @@
+﻿using AuthenticationService.Application.DTOs;
+using AuthenticationService.Domain.Entities;
+using AuthenticationService.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthenticationController(AuthDbContext context, IConfiguration config) : ControllerBase
+{
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(Register userRegister)
+    {
+        if (userRegister == null) return BadRequest();
+
+        var getUser = await context.ApplicationUsers.FirstOrDefaultAsync(u => u.Email == userRegister.Email);
+
+        if (getUser != null) return BadRequest(userRegister.Email);
+
+        var entity = context.ApplicationUsers.Add(new ApplicationUser()
+        {
+            Nome = userRegister.NomeCompleto,
+            Email = userRegister.Email,
+            Senha = BCrypt.Net.BCrypt.HashPassword(userRegister.Senha),
+        }).Entity;
+
+        await context.SaveChangesAsync();
+
+        context.UserRoles.Add(new UserRole()
+        {
+            ApplicationUserId = entity.Id,
+            Role = "Usuário padrão"
+        });
+
+        await context.SaveChangesAsync();
+
+        return Ok("Usuário cadastrado com sucesso!");
+    }
+
+    [HttpPost("login")]
+    public Login(Login userLogin)
+    {
+
+    }
+}
