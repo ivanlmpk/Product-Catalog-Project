b385b017d38ae5c0a74a21dbe27414c7be3b6d94
ivanlmpk <ivanlempek@hotmail.com>
2024-09-03 21:43:03 -0300

    feat:  Adicionado uma chave de autenticação no appsettings. Terminado o método de login com toda a logica para verificar a senha usando o BCrypt e para criar um token de acesso único apartir da chave criada no appsettings.

--- AuthenticationService/API/Controllers/AuthenticationController.cs
+++ AuthenticationService/API/Controllers/AuthenticationController.cs
@@ -3,6 +3,7 @@ using AuthenticationService.Domain.Entities;
 using AuthenticationService.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 
 namespace AuthenticationService.API.Controllers;
 
@@ -13,11 +14,13 @@ public class AuthenticationController(AuthDbContext context, IConfiguration conf
     [HttpPost("register")]
     public async Task<IActionResult> Register(Register userRegister)
     {
-        if (userRegister == null) return BadRequest();
+        if (userRegister == null) 
+            return BadRequest();
 
         var getUser = await context.ApplicationUsers.FirstOrDefaultAsync(u => u.Email == userRegister.Email);
 
-        if (getUser != null) return BadRequest(userRegister.Email);
+        if (getUser != null) 
+            return BadRequest(userRegister.Email);
 
         var entity = context.ApplicationUsers.Add(new ApplicationUser()
         {
@@ -40,8 +43,27 @@ public class AuthenticationController(AuthDbContext context, IConfiguration conf
     }
 
     [HttpPost("login")]
-    public Login(Login userLogin)
+    public async Task<IActionResult> Login(Login userLogin)
     {
+        if (string.IsNullOrWhiteSpace(userLogin.Email) || string.IsNullOrWhiteSpace(userLogin.Senha))
+            return BadRequest("O campo de e-mail ou senha está vazio.");
 
+        var getUserLogin = await context.ApplicationUsers.FirstOrDefaultAsync(u => u.Email == userLogin.Email);
+
+        if (getUserLogin == null) 
+            return BadRequest("Não foi encontrado um usuário para esse e-mail e senha.");
+
+        var verifyPassword = BCrypt.Net.BCrypt.Verify(userLogin.Senha, getUserLogin.Senha);
+
+        if (!verifyPassword)
+            return NotFound("Senha inválida.");
+
+        var getRole = await context.UserRoles.FirstOrDefaultAsync(r => r.ApplicationUserId == getUserLogin.Id);
+
+        string key = $"{config["Authentication:Key"]}.{getUserLogin.Nome}.{getUserLogin.Id}";
+
+        string accessToken = Convert.ToBase64String(Encoding.UTF8.GetBytes(key));
+
+        return Ok($"Token de acesso: {accessToken}");
     }
 }
