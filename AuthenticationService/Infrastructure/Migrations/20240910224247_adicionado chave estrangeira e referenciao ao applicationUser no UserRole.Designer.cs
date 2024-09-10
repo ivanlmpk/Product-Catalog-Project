cd1b8294f6943ed030b09aba9b8196014f34f15e
ivanlmpk <ivanlempek@hotmail.com>
2024-09-10 19:52:22 -0300

    feat: Adicionado migration da chave estrangeira.

--- AuthenticationService/Infrastructure/Migrations/20240910224247_adicionado chave estrangeira e referenciao ao applicationUser no UserRole.Designer.cs
+++ AuthenticationService/Infrastructure/Migrations/20240910224247_adicionado chave estrangeira e referenciao ao applicationUser no UserRole.Designer.cs
@@ -3,6 +3,7 @@ using AuthenticationService.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuthenticationService.Infrastructure.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240910224247_adicionado chave estrangeira e referenciao ao applicationUser no UserRole")]
+    partial class adicionadochaveestrangeiraereferenciaoaoapplicationUsernoUserRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AuthenticationService/Infrastructure/Migrations/20240910224247_adicionado chave estrangeira e referenciao ao applicationUser no UserRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthenticationService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class adicionadochaveestrangeiraereferenciaoaoapplicationUsernoUserRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_ApplicationUserId",
+                table: "UserRoles",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_ApplicationUsers_ApplicationUserId",
+                table: "UserRoles",
+                column: "ApplicationUserId",
+                principalTable: "ApplicationUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_ApplicationUsers_ApplicationUserId",
+                table: "UserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserRoles_ApplicationUserId",
+                table: "UserRoles");
+        }
+    }
+}
