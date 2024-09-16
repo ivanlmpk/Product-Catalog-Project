17e7883b7d53cdfee60d72d6b3e039163b9dabc5
ivanlmpk <ivanlempek@hotmail.com>
2024-09-16 01:11:01 -0300

    feat: Adicionado cascadingAuthenticationState ao routes.razor e ajustado routeView para AuthorizeRouteView. Adicionado authorizing com mensagem que é exibido enquanto operações assincronas estão em execução.

--- Product-Catalog/Product-Catalog.Client/Program.cs
+++ Product-Catalog/Product-Catalog.Client/Program.cs
@@ -2,10 +2,14 @@ using Blazored.LocalStorage;
 using ExternalServices.Helpers;
 using ExternalServices.Services.Authentication;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Product_Catalog.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<Routes>("#routes");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7079/") });
 builder.Services.AddAuthorizationCore();
