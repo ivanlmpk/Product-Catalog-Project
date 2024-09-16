5fc69fbe2a94f7d717f34d9ff5739515977e29c4
ivanlmpk <ivanlempek@hotmail.com>
2024-09-16 00:36:29 -0300

    feat: Adicionado métodos SetClaimPrincipal e UpdateAuthenticationState complementado as outras funções referentes ao estado de autenticação do usuário.

--- ExternalServices/Helpers/CustomAuthenticationStateProvider.cs
+++ ExternalServices/Helpers/CustomAuthenticationStateProvider.cs
@@ -15,7 +15,7 @@ public class CustomAuthenticationStateProvider(LocalStorageService localStorageS
             return await Task.FromResult(new AuthenticationState(anonymous));
 
         var deserializeToken = Serializations.DeserializeJsonString<UserSession>(stringToken);
-        if (deserializeToken != null)
+        if (deserializeToken == null)
             return await Task.FromResult(new AuthenticationState(anonymous));
 
         var getUserClaims = DecryptToken(deserializeToken.Token!);
@@ -29,14 +29,49 @@ public class CustomAuthenticationStateProvider(LocalStorageService localStorageS
 
     private static CustomUserClaims DecryptToken(string jwtToken)
     {
-        if (string.IsNullOrEmpty(jwtToken)) return new CustomUserClaims();
+        if (string.IsNullOrEmpty(jwtToken)) 
+            return new CustomUserClaims();
 
         var handler = new JwtSecurityTokenHandler();
         var token = handler.ReadJwtToken(jwtToken);
+
         var userId = token.Claims.FirstOrDefault(_ => _.Type == ClaimTypes.NameIdentifier);
         var name = token.Claims.FirstOrDefault(_ => _.Type == ClaimTypes.Name);
         var email = token.Claims.FirstOrDefault(_ => _.Type == ClaimTypes.Email);
         var role = token.Claims.FirstOrDefault(_ => _.Type == ClaimTypes.Role);
+
         return new CustomUserClaims(userId!.Value, name!.Value, email!.Value, role!.Value);
+    }
+    public static ClaimsPrincipal SetClaimPrincipal(CustomUserClaims claims)
+    {
+        if (claims.Email == null)
+            return new ClaimsPrincipal();
+
+        return new ClaimsPrincipal(new ClaimsIdentity(
+            new List<Claim>
+            {
+                new(ClaimTypes.NameIdentifier, claims.Id!),
+                new(ClaimTypes.Name, claims.Nome!),
+                new(ClaimTypes.Email, claims.Email!),
+                new(ClaimTypes.Role, claims.Role!),
+            }, "JwtAuth")
+        );
+    }
+
+    public async Task UpdateAuthenticationState(UserSession userSession)
+    {
+        var claimsPrincipal = new ClaimsPrincipal();
+        if (userSession.Token != null || userSession.RefreshToken != null)
+        {
+            var serializeSession = Serializations.SerializeObj(userSession);
+            await localStorageService.SetToken(serializeSession);
+            var getUserClaims = DecryptToken(userSession.Token!);
+            claimsPrincipal = SetClaimPrincipal(getUserClaims);
+        }
+        else
+        {
+            await localStorageService.RemoveToken();
+        }
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
     }
 }
