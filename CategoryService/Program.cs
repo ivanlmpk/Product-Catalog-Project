f9f9c329c9d4eaef3b91b6acf272e1ca6786f913
ivanlmpk <ivanlempek@hotmail.com>
2024-09-24 02:44:41 -0300

    feat: Adicionado todos end-points de categoria. Injetado novo serviço de categoria ao container de injeção de dependencia.

--- CategoryService/Program.cs
+++ CategoryService/Program.cs
@@ -1,4 +1,6 @@
+using Application.CategoryService.Application.Interfaces;
 using Application.CategoryService.Infrastructure.Data;
+using Application.CategoryService.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +16,8 @@ builder.Services.AddDbContext<CategoryDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Database") ??
         throw new InvalidOperationException("Erro: Sua conexão não foi encontrada!"));
 });
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 var app = builder.Build();
 
