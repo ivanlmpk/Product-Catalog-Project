c0bb2281b3b12ca3da2e63fda1ebe07fc34da604
ivanlmpk <ivanlempek@hotmail.com>
2024-09-23 23:20:53 -0300

    feat: Adicionado DTO para categoria. Refatorado pastas de paginas no Client. Adicionado arquivo de pagina para exibição de categorias.

---
+++ 1-BaseDTOs/Category/CategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace _1_BaseDTOs.Category;
+
+public class CategoryDTO
+{
+    public int Id { get; set; }
+    public string? Titulo { get; set; }
+    public string? Descricao { get; set; }
+    public int UserId { get; set; }
+    public DateTime Data { get; set; }
+}
