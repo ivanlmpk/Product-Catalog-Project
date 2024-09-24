f265bfb8abc119bd75d2456edf9814ee93bc72fe
ivanlmpk <ivanlempek@hotmail.com>
2024-09-24 01:32:21 -0300

    feat: Adicionado implementação concreta dos serviços de categoria da API.

--- CategoryService/Infrastructure/Repositories/CategoryService.cs
+++ CategoryService/Infrastructure/Repositories/CategoryService.cs
@@ -1,6 +1,64 @@
-﻿namespace Application.CategoryService.Infrastructure.Repositories
+﻿using Application.CategoryService.Application.Interfaces;
+using Application.CategoryService.Domain.Entities;
+using Application.CategoryService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.CategoryService.Infrastructure.Repositories
 {
-    public class CategoryService
+    public class CategoryService : ICategoryService
     {
+        private readonly CategoryDbContext _context;
+
+        public CategoryService(CategoryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            if (category == null) throw new ArgumentException("Categoria inválida.");
+
+            _context.Categories.Add(category);
+
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            if (id == 0) throw new ArgumentException("ID inválido.");
+
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null) throw new KeyNotFoundException("Categoria não encontrada.");
+
+            return category;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            if (category == null) throw new ArgumentException("Produto inválido.");
+
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            if (id == 0) throw new ArgumentException("ID inválido.");
+
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null) return false;
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 }
