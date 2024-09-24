3a5e2d50917e7487d14c48a237b5bc9d790b1b3e
ivanlmpk <ivanlempek@hotmail.com>
2024-09-24 01:29:00 -0300

    feat: Adicionado interface de categoria na API.

---
+++ CategoryService/Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using Application.CategoryService.Domain.Entities;
+
+namespace Application.CategoryService.Application.Interfaces;
+
+public interface ICategoryService
+{
+    Task<IEnumerable<Category>> GetAllAsync();
+    Task<Category> GetByIdAsync(int id);
+    Task<Category> CreateAsync(Category category);
+    Task<Category> UpdateAsync(Category category);
+    Task<bool> DeleteAsync(int id);
+}
---
+++ CategoryService/Infrastructure/Repositories/CategoryService.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.CategoryService.Infrastructure.Repositories
+{
+    public class CategoryService
+    {
+    }
+}
